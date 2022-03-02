34aa889e8f4e1b6d1976771a51d297b803688754
edilmarflores <99268191+edilmarflores@users.noreply.github.com>
2022-03-02 23:56:33 +0800

    Add files via upload

---
+++ Array JS/index.js
@@ -0,0 +1,6 @@
+var namesArray = ["Steve", "John", "Betty", "Morty", "Rick", "Alan", "Sarah"]
+
+console.log(namesArray [2]);
+console.log(namesArray [6]);
+console.log(namesArray.length);
+console.log('the index of Steve is 0' + namesArray);
