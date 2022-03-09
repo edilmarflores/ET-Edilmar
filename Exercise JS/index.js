858616bf313c8793995256cce78dee5dd4ee72f2
edilmarflores <99268191+edilmarflores@users.noreply.github.com>
2022-03-09 21:18:24 +0800

    Add files via upload

---
+++ Exercise JS/index.js
@@ -0,0 +1,76 @@
+// if(2 === 4){
+//     console.log("Two is equal to four")
+// } else if(2 === 5){
+//     console.log("Two is equal to five")
+// } else if(2 === 3){
+//     console.log("two is equal to kahoy")
+// } else {
+//     console.log("Is this effective?")
+// }
+
+// Comparison Operator
+   // >, <, <=, >=, ==, ===, !=, !== 
+
+// if ("2" !== 2){
+//     console.log("This is goona be equal")
+// }
+
+// truthy and falsey
+    // falsey              Truthy
+        // 0                
+        // ""
+        // null
+        // undefined
+        // false
+        // NaN
+
+// if(0){
+//     console.log("this is gonna be truthy")
+// } else {
+//     console.log("this is gonna be falsey")
+// }
+
+//  "This is gonna be true because inside has a value"
+// if([1, 2, 3, 4, 5]){
+//     console.log("this is gonna be truthy")
+// } else {
+//     console.log("this is gonna be falsey")
+// }
+
+
+
+// Logic Operators
+    // && - and "check (base) both the value"
+    // || - or  "check (base) the first value"
+    // !  - not (equal) the first and last value
+
+// if(2 === 2 && 3 === 3 && 2 !== 3){
+//     console.log("It is gonna be work")
+// } else {
+//     console.log("It is not gonna work")
+// }
+
+// Order of operations  + - * /
+
+// SWITH STATEMENT
+var color = "grey"
+
+switch(color){
+    case "red":
+        console.log("This is RED")
+        break
+    case "blue":
+        console.log("This is BLUE")
+        break
+    case "yellow":
+        console.log("This is YELLOW")
+        break
+    default:
+        console.log("This is not belong to the choices")
+
+}
+
+
+
+
+
