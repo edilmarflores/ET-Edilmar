fa5a438e03224d2e1fb01345647fcc8f33ffb7d1
edilmarflores <99268191+edilmarflores@users.noreply.github.com>
2022-03-15 22:51:44 +0800

    Add files via upload

---
+++ If Statement Olympic/index.js
@@ -0,0 +1,48 @@
+// Preliminaries
+
+// 1.
+// if (5 > 3){
+//     console.log("is greater than")
+// }
+
+// // 2.
+// if ("the lenght of cat is 3"){
+//     console.log("is the length")
+// }
+
+// // 3.
+// if ("cat = dog"){
+//     console.log("not the same")
+// } else {
+//     console.log("the same")
+// }
+
+// Bronze Medal
+// 1.
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if("theNameOfThePersonInObject"){
+//     console.log("Bobby is allowed to go to the movie")
+// }
+
+// 2.
+// var person = {
+//     name: "Bobby",
+//     age: 12
+// }
+
+// if("theNameOfThePersonInObject")
+// console.log("Bobby's Name Starts with B")
+
+// 3.
+
+var person = {
+    name: "Bobby",
+    age: 19
+  }
+
+if("theNameOfThePersonInObject")
+console.log("Bobby's age is older than 18")
